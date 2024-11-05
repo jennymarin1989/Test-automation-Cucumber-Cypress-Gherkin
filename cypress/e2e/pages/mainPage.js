95cdd3abe131d4a9941e2693b73b916f50cd5ec1
jennymarin1989 <jennymarin1989@hotmail.comm>
2024-11-06 00:34:16 +0100

    Refactor of selectors into constants

--- cypress/e2e/pages/mainPage.js
+++ cypress/e2e/pages/mainPage.js
@@ -1,30 +1,35 @@
 import { CommonPage } from './commonPage';
 
+const dropDownFunnelElement = '[data-test="product-sort-container"]';
+const productElementFromList = '[data-test="inventory-item"]';
+const productElementDescription = '[data-test="inventory-item-description"]';
+const productElementPrice = '[data-test="inventory-item-price"]';
+
 export class MainPage extends CommonPage {
   checkFilterOptionSelected(optionValue, optionText) {
-    cy.get('[data-test="product-sort-container"] option:selected')
+    cy.get(`${dropDownFunnelElement} option:selected`)
       .should('have.text', optionText)
       .and('have.value', optionValue);
   }
   checkFirstProductNameAndPrice(productName, productPrice) {
-    cy.get('[data-test="inventory-item"]')
+    cy.get(productElementFromList)
       .first()
       .within(() => {
-        cy.get('[data-test="inventory-item-description"]').should('contain', productName);
-        cy.get('[data-test="inventory-item-price"]').should('contain', productPrice);
+        cy.get(productElementDescription).should('contain', productName);
+        cy.get(productElementPrice).should('contain', productPrice);
       });
   }
 
   checkLastProductNameAndPrice(productName, productPrice) {
-    cy.get('[data-test="inventory-item"]')
+    cy.get(productElementFromList)
       .last()
       .within(() => {
-        cy.get('[data-test="inventory-item-description"]').should('contain', productName);
-        cy.get('[data-test="inventory-item-price"]').should('contain', productPrice);
+        cy.get(productElementDescription).should('contain', productName);
+        cy.get(productElementPrice).should('contain', productPrice);
       });
   }
 
   selectFilterOption(option) {
-    cy.get('[data-test="product-sort-container"]').select(option);
+    cy.get(dropDownFunnelElement).select(option);
   }
 }
